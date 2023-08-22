6b1b26e7f6fc262ba1fb8c418cf7c23928df0dae
alhassanyoussef <hyoussef650@.com>
2023-08-22 16:51:03 +0300

    this is handling s& c& %

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - function that produces output according to format
+ * @format: pointer
+ * Return: (0)
+ */
+
+int _printf(const char *format, ...)
+{
+	int p_char = 0;
+	va_list a_rgs;
+
+	if (format == NULL)
+		return (-1);
+	va_start(a_rgs, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{	write(1, format, 1);
+			p_char++; }
+		else
+		{
+			format++;
+			if (*format == '\n')
+			break;
+			if (*format == '%')
+			{	write(1, format, 1);
+				p_char++; }
+			else if (*format == 'c')
+			{	char ch = va_arg(a_rgs, int);
+
+				write(1, &ch, 1);
+				p_char++; }
+			else if (*format == 's')
+			{	char *star = va_arg(a_rgs, char*);
+				int len = 0;
+
+				while (star[len] != '\0')
+					len++;
+				write(1, star, len);
+				p_char += len; }
+		format++; }
+	}
+
+	va_end(a_rgs);
+	return (p_char);
+}
